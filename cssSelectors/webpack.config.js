2d76c68bfb36254e55ba0ea93e4a2723196f9bd7
Aliaksei <sladkiy.aleksey@gmail.com>
2023-06-17 19:14:33 +0200

    feat: add project

---
+++ cssSelectors/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'CSS Selectors',
+      favicon: './src/favicon.ico',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
